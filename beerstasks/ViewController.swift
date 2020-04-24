886680a45fe8352f20852f662b66403e7efe666a
Guilherme Cunha <guizera7@gmail.com>
2020-04-24 17:42:33 -0300

    Initial Commit

---
+++ beerstasks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  beerstasks
+//
+//  Created by José Alves da Cunha on 24/04/20.
+//  Copyright © 2020 José Alves da Cunha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
